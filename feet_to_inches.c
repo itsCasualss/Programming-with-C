75971f4c6ca6dfbca13948002318d08d68d5c76c
itsCasualss <austinpetit@gmail.com>
2026-01-22 17:15:15 -0500

    Update feet_to_inches.c
    Working Code, 
    
    %u is specifically for unsigned integers

--- feet_to_inches.c
+++ feet_to_inches.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(void)
+{
+  unsigned short int inches = 3435;
+  unsigned short int feet_inch;
+
+unsigned short int inches_remainder;
+  float feet_float;
+  //conversions
+  feet_inch=inches/12;
+  inches_remainder=inches%12;
+    feet_float =inches/12.0;
+
+  //printing results
+  printf("%u inches is %u feet, %u inches. \n", inches, feet_inch, inches_remainder);
+  printf("As a float, that's %0.2f feet. \n", feet_float);
+
+getchar();
+return 0;
+}
