b673780684b70fc94d067f7433cbac5c89516ea5
itsCasualss <austinpetit@gmail.com>
2026-01-22 17:25:31 -0500

    Update feet_to_inches.c
    // line 13

--- feet_to_inches.c
+++ feet_to_inches.c
@@ -10,7 +10,7 @@ unsigned short int inches_remainder;
   //conversions
   feet_inch=inches/12;
   inches_remainder=inches%12;
-    feet_float =inches/12.0;
+    feet_float =inches/12; //removing the ".0"  changes the output from a 0.25 remainder to the only 286 feet
 
   //printing results
   printf("%u inches is %u feet, %u inches. \n", inches, feet_inch, inches_remainder);
