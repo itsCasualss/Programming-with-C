e98fea2d724dce166c2cf6cda1c0ec466fdafef9
itsCasualss <austinpetit@gmail.com>
2026-04-16 11:10:57 -0400

    Create mallock.c

---
+++ lectures/Module 9/mallock.c
@@ -0,0 +1,8 @@
+void *x // void pointer
+int memsize;
+x = malloc(memsize)
+
+//when you are done with the block, return it to the pool using the free() function.
+free (x);
+
+Every call to malloc() must be balanced by a call to free(). a missing or double free() call is considered a bug (a missing free() statement creates a memory leak)
